98eef76256e3bf400964031439ba92c01f581902
recolte00 <yukiyako2.lulu1920@gmail.com>
2021-02-02 07:17:26 +0900

    first commit

---
+++ ChromeExtention/02_Show_TabsInfo/popup.js
@@ -0,0 +1,13 @@
+'use strict';
+
+chrome.tabs.query({active:true,lastFocusedWindow: true},function(tabs){
+    var i;
+    var results = document.getElementById('results');
+    var titles = [];
+    for(i=0;i<tabs.length;i++){
+        titles.push(tabs[i].title);
+        //console.log(tabs[i].title);
+    }
+    results.value = titles.join("\n");
+    results.select();
+});
